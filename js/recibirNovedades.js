ddd27a4a41f5431a6ae5b4f5bf4931a59a6adf27
Geraldina <geralbm@gmail.com>
2025-11-13 18:46:02 -0300

    Estilos

---
+++ js/recibirNovedades.js
@@ -0,0 +1,7 @@
+ //Funcion que mails para que se les envie la novedeades.
+
+ let evenNove = document.getElementById("btnHome")
+ evenNove = addEventListener ("click",completarNov)
+ function completarNov() {
+    console.log ("HOLA PRUEBA")
+ }
